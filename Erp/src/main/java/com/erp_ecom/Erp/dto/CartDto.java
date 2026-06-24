392b8dea1456700a1d230f8a59137623aa3cf422
Sahibpreet Singh <157112392+Sahibpreetsingh2@users.noreply.github.com>
2026-06-25 03:11:39 +0530

    Add files via upload

---
+++ Erp/src/main/java/com/erp_ecom/Erp/dto/CartDto.java
@@ -0,0 +1,12 @@
+package com.erp_ecom.Erp.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CartDto {
+
+    private Long productId;
+    private Integer quantity;
+}
